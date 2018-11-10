1e60b06cae1b774bd8f80b43adfa9bacce872a92
Gustavo Delgado <gus@gustavos-mbp-2.lan>
2018-11-10 00:51:28 -0600

    Token in edit text

--- app/src/main/java/grin/com/challenge/MainActivity.java
+++ app/src/main/java/grin/com/challenge/MainActivity.java
@@ -1,13 +1,53 @@
 package grin.com.challenge;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEditToken;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditToken = findViewById(R.id.edit_token);
+
+        getToken();
+    }
+
+
+    private void getToken() {
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("CHALLENGE", "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+                        mEditToken.setText(token);
+
+                        // Log and toast
+                        String msg = "Token: " + token;
+                        Log.d("CHALLENGE", msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
---
+++ app/src/main/java/grin/com/challenge/MyFirebaseMessagingService.java
@@ -0,0 +1,33 @@
+package grin.com.challenge;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        super.onDeletedMessages();
+    }
+
+    @Override
+    public void onMessageSent(String s) {
+        super.onMessageSent(s);
+    }
+
+    @Override
+    public void onSendError(String s, Exception e) {
+        super.onSendError(s, e);
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+    }
+}
